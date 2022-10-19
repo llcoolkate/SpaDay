3ce56d69f20943db87dcdd08763daa34e6c1adea
LLCoolKate <107154088+llcoolkate@users.noreply.github.com>
2022-10-19 18:52:05 -0500

    init

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/user/add")]
+        public IActionResult Add()
+        {
+            //ViewBag.user = newUser;
+            return View();
+        }
+        public IActionResult SubmitAddUserForm(UserController newUser, string verify)
+        {
+            //add form submission handling code here 
+
+        }
+    }
+}
---
+++ SpaDay/Models/User.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+
+namespace SpaDay.Models
+{
+    public class User 
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+}
